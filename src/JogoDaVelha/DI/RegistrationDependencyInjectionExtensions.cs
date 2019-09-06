e6e98c3d2638c1b6ba73223e2ad117a783af649e
Henrique Burle Melgaço <henrique.melgaco@animaeducacao.com.br>
2019-09-05 23:22:15 -0300

    Criação da estrutura inicial do projeto

---
+++ src/JogoDaVelha/DI/RegistrationDependencyInjectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha.DI
+{
+    public static class RegistrationDependencyInjectionExtensions
+    {
+        public static void AddRegistrationDependencies(this IServiceCollection services, IConfiguration configuration = null)
+        {
+            services.AddSingleton(configuration);
+
+            RegisterRepositories(services, configuration);
+            RegisterServices(services);
+            //services.AddSingleton<IObjectConverter, ObjectConverter>();
+        }
+
+        private static void RegisterRepositories(IServiceCollection services, IConfiguration configuration = null)
+        {
+            //services.AddTransient<ICachedRepository, CachedRepository>();
+
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            //services.AddScoped<IGameService, GameService>();
+
+        }
+
+
+    }
+}
